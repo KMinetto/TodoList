63e1983525ed0e8c2abe94f99a385538734551da
Kilian MINETTO <killian.minetto@gmail.com>
2021-09-06 01:03:37 +0200

    Ajout du Header

---
+++ src/components/Header/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="header-container">
+    <p>{{ title }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  data() {
+    return {
+      title: 'La Todo-List'
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .header-container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100px;
+    background: #333333;
+    color: #f1f1f1;
+  }
+
+  p {
+    font-size: 30px;
+  }
+</style>
